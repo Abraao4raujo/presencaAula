06b296a32f909d31b26b8b89d923f0bbff8eaede
Abraão Araujo <96948947+Abraao4raujo@users.noreply.github.com>
2023-05-12 12:54:32 -0300

    corrigindo problema com fetch

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const dados = document.getElementById('dados')
 
 async function getGeolocation(){
   try{
-    const response = await fetch('http://ip-api.com/json')
+    const response = await fetch('http://ip-api.com/json', {
+      referrerPolicy: "unsafe_url" 
+    })
     const data = await response.json()
 
     dados.innerHTML = `<br> ${data.query} <br> ${data.as} <br> ${data.org} <br> ${data.lat} <br> ${data.lon} <br> ${data.city}`
